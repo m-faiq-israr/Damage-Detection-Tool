74247f827f21124950561cdf258200c0f4006cce
GitHub Action <action@github.com>
2026-08-13 23:30:17 +0500

    production_changes_5

--- damage_detection_tool/damage_detection_tool/settings.py
+++ damage_detection_tool/damage_detection_tool/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-aslzii_!qj35(_%w^w9c@xw+^slqs&+yfqj&ffa&#@qes91ecb"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
